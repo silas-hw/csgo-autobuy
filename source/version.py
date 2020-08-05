65fe13f7742c05db0cddb4063243787adc3172d1
Silas HW <silas.hayes.williams@gmail.com>
2020-08-05 02:37:20 +0100

    Add properties to exe

---
+++ source/version.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(6, 1, 7601, 17514),
+    prodvers=(6, 1, 7601, 17514),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'silas-hw'),
+        StringStruct(u'FileDescription', u'Generates a csgo buy bind'),
+        StringStruct(u'FileVersion', u'0.1'),
+        StringStruct(u'InternalName', u'csgo-autobuy'),
+        StringStruct(u'LegalCopyright', u'GPL-3.0'),
+        StringStruct(u'OriginalFilename', u'csgo-autobuy.exe'),
+        StringStruct(u'ProductName', u'Autobuy'),
+        StringStruct(u'ProductVersion', u'0.1')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
